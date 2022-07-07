07a6563570427e1f1bd983e24c37ac5aa2a852f5
Ivan Ivanchenko <ivanisovich@yandex.ru>
2022-07-07 22:21:22 +0300

    homework 6 completed

--- script.js
+++ script.js
@@ -1,46 +1,97 @@
-const app = new Vue({
-  el: "#app",
-  data: {
-    goods: [],
-    filteredGoods: [],
-    searchLine: "",
-  },
-  methods: {
-    makeGETRequest(url, callback) {
-      return new Promise((resolve, reject) => {
-        var xhr;
-        if (window.XMLHttpRequest) {
-          xhr = new XMLHttpRequest();
-        } else if (window.ActiveXObject) {
-          xhr = new ActiveXObject("Microsoft.XMLHTTP");
-        }
-        xhr.onreadystatechange = function () {
-          if (xhr.readyState === 4) {
-            callback(xhr.responseText);
-          }
-        };
-        xhr.open("GET", url, true);
-        xhr.send();
+const BASE_URL =
+  "https://raw.githubusercontent.com/GeekBrainsTutorial/online-store-api/master/responses/";
+const GET_GOODS_ITEMS = `${BASE_URL}catalogData.json`;
+const GET_BASKET_GOODS_ITEMS = `${BASE_URL}getBasket.json`;
 
-        resolve("resolved");
-      });
+function service(url) {
+  return fetch(url).then((res) => res.json());
+}
+
+function init() {
+  const app = new Vue({
+    el: '#root',
+    data: {
+      items: [],
+      filteredItems: [],
+      search: '',
+      cardIsVisiable: false
     },
-  },
-  mounted() {
-    const API_URL =
-      "https://raw.githubusercontent.com/GeekBrainsTutorial/online-store-api/master/responses";
+    methods: {
+      setVisiability() {
+        this.cardIsVisiable = !this.cardIsVisiable
+      },
+      fetchGoods() {
+        service(GET_GOODS_ITEMS).then((data) => {
+          this.items = data;
+          this.filteredItems = data;
+        });
+      },
+      filterItems() {
+        this.filteredItems = this.items.filter(({ product_name }) => {
+          return product_name.match(new RegExp(this.search, 'gui'))
+        })
+      },
+    },
+    computed: {
+      calculatePrice() {
+        return this.filteredItems.reduce((prev, { price }) => {
+          return prev + price;
+        }, 0)
+      }
+    },
+    mounted() {
+      this.fetchGoods();
+    }
+  })
 
-    this.makeGETRequest(`${API_URL}/catalogData.json`, (goods) => {
-      this.goods = JSON.parse(goods);
-      this.filteredGoods = JSON.parse(goods);
-    });
-  },
+const cart = Vue.component("cart", {
+    data() {
+      return {
+        cartItems: [],
+      };
+    },
+  
+    template: `
+        <div class="fixed-area">
+           <div class="cart-card">
+              <div class="cart-card__header">
+                 <h1 class="cart-card__header__title">cart card</h1>
+                 <div class="cart-card__header__delete-icon"
+                    v-on:click="$emit('closeclick')"
+                 ></div>
+              </div>
+              <div class="cart-card__content">
+                 content
+              </div>
+           </div>
+        </div>
+      `,
+});  
+
+const customButton = Vue.component("custom-button", {
+  template: `
+      <button class="search-button" type="button" v-on:click="$emit('click')">
+         <slot></slot>
+      </button>
+    `,
 });
 
-const cart = new Vue({
-  el: "#cart",
-  data: {
-    isVisibleCart : "true"
-  },
+
+
+const goodsItem = Vue.component("goods-item", {
+  props: ["item"],
+  template: `
+      <div class="goods-item">
+         <h3>{{ item.product_name }}</h3>
+         <p>{{ item.price }}</p>
+      </div>
+    `,
+});
+
+service(GET_BASKET_GOODS_ITEMS).then((data) => {
+  cart.cartItems = data
 })
 
+}
+
+window.onload = init;
